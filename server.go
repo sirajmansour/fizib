c2807c215e6b1b61510b9ca4561a5b9475ccc251
promiselb <107135539+promiselb@users.noreply.github.com>
2022-08-18 15:40:13 +0300

    Update server.go

--- server.go
+++ server.go
@@ -59,16 +59,19 @@ func (srv *Server) SignUp() http.HandlerFunc {
 
 			err = os.Mkdir(dataBasePath+path+"dir", os.ModePerm)
 			if err != nil {
+				fmt.Fprint(w, errAlreadyOccupiedGmail.Error())
 				return
 			}
 
 			jsonFile, err := os.Create(dataBasePath + path + "dir/" + "data.json")
 			if err != nil {
+				fmt.Fprint(w, errAlreadyOccupiedGmail.Error())
 				return
 			}
 
 			err = json.NewEncoder(jsonFile).Encode(user)
 			if err != nil {
+				fmt.Fprint(w, err.Error())
 				return
 			}
 
@@ -82,5 +85,94 @@ func (srv *Server) SignUp() http.HandlerFunc {
 		default:
 			fmt.Fprintf(w, "No other methods are supported so far.")
 		}
+	}
+}
+
+func (srv *Server) LogIn() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles(htmlPagesPath + "logIn.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch r.Method {
+
+		case "GET":
+			t.Execute(w, logInRespond{})
+		case "POST":
+
+			if err := r.ParseForm(); err != nil {
+				fmt.Fprintf(w, "ParseForm() err: %v", err)
+				return
+			}
+
+			gmail = Gmail(r.FormValue("gmail"))
+			password = Password(r.FormValue("password"))
+
+			for _, err := range []error{gmail.Check(), password.Check()} {
+				if err != nil {
+					t.Execute(w, logInRespond{
+						Gmail:    gmail,
+						Password: password,
+						Error:    "Error: " + err.Error(),
+					})
+					return
+				}
+			}
+			user := &User{}
+			path := string(gmail)
+
+			jsonFile, err := os.Open(dataBasePath + path + "dir/" + "data.json")
+			if err != nil {
+				t.Execute(w, logInRespond{
+					Gmail:    gmail,
+					Password: password,
+					Error:    "Error: " + errNoRegisteredOrDeletedAccount.Error(),
+				})
+				return
+			}
+
+			err = json.NewDecoder(jsonFile).Decode(user)
+			if err != nil {
+				t.Execute(w, logInRespond{
+					Gmail:    gmail,
+					Password: password,
+					Error:    "Error: " + errLostOrDeletedData.Error(),
+				})
+				return
+			}
+
+			err = password.Compare(user.Password)
+			if err != nil {
+				t.Execute(w, logInRespond{
+					Gmail:    gmail,
+					Password: password,
+					Error:    "Error: " + err.Error(),
+				})
+				return
+			}
+
+			err = jsonFile.Close()
+			if err != nil {
+				t.Execute(w, logInRespond{
+					Gmail:    gmail,
+					Password: password,
+					Error:    "Error: " + err.Error(),
+				})
+				log.Fatal(err)
+			}
+
+			redirectURL := fmt.Sprintf(`http://localhost%s/login/%s`, Port, gmail)
+			http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+
+		default:
+			fmt.Fprintf(w, "No other methods are supported so far.")
+		}
+	}
+}
+
+func (srv *Server) Welcome() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		http.ServeFile(w, r, `C:\HtmlPages\html\welcome.html`)
 	}
 }
